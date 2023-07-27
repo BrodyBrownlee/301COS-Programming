23540d309e400e5505f13ea16f9a05961677a6e4
BrodyBrownlee <brodybrownlee@mmc.school.nz>
2023-07-27 20:10:16 +1200

    worked on projectiles

--- Assets/Scripts/Player_Shoot.cs
+++ Assets/Scripts/Player_Shoot.cs
@@ -10,7 +10,7 @@ public class Player_Shoot : MonoBehaviour
     private GameObject enemyHeight;
     public float shootDelay = 1f;
     public float timer = 1f;
-
+    private float rotation;
     private void Start()
     {
         projectileSpawn = gameObject.transform.Find("Projectileposition").gameObject;
@@ -20,25 +20,28 @@ public class Player_Shoot : MonoBehaviour
     {
         timer += Time.deltaTime;
         //if a directional key is being pressed create a bullet and rotate based on the direction
-        if (shootDelay < timer)
+        
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
         {
-            if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+            if (shootDelay < timer)
             {
                 GameObject newBullet = Instantiate(pfBullet);
+                if( = 180)
+                {
+
+                }
                 newBullet.transform.position = projectileSpawn.transform.position;
-            
                 newBullet.transform.Rotate(0,0, getRotation());
                 timer = 0;
             }
         }
-
     }
     //finding the rotation based on the key press
-    private float getRotation()
+    private float getRotation(float rotation)
     { 
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            return 180;
+            return 180; 
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
@@ -47,6 +50,10 @@ public class Player_Shoot : MonoBehaviour
         if (Input.GetKey(KeyCode.RightArrow))
         {
             return 270;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            return 0;
         }
         else
         {
