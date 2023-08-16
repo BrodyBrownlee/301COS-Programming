f2b740e2d048ed72a915636a357d7403d18d2159
BrodyBrownlee <brodybrownlee@mmc.school.nz>
2023-08-16 14:26:27 +1200

    changing coords of things

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,7 +11,7 @@ public class Enemy : MonoBehaviour
     public float moveSpeed;
     public float attackRange;
     private bool canAttack;
-    public  GameObject target;
+    public GameObject target;
     public NavMeshAgent agent;
     private Vector2 directionToTarget;
 
@@ -21,7 +21,6 @@ public class Enemy : MonoBehaviour
         target = GameObject.Find("Player");
         hp = 3;
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -34,61 +33,33 @@ public class Enemy : MonoBehaviour
             //destroy enemy; 
             Debug.Log("enemy dead");
             Destroy(gameObject);
-            if(enemySpawn.spawnerScript != null) {
+            if (enemySpawn.spawnerScript != null)
+            {
                 //subtract one from the number of enemies
                 enemySpawn.spawnerScript.numberOfEnemies--;
                 return;
             }
-          
         }
-      
     }
     private void FixedUpdate()
     {
-        //if the enemy does not have a target
-        if (target != null)
+        if (agent.destination != null)
         {
-            //set the destination of the agent to the target position
-            agent.SetDestination(target.transform.position);
+            //if the enemy does not have a target
+            if (target != null)
+            {
+                //set the destination of the agent to the target position
+                agent.SetDestination(target.transform.position);
+            }
         }
-
-        /*if(!canAttack)
-        {
-            gameObject.GetComponent<Rigidbody>().velocity = new Vector3(directionToTarget.x, gameObject.transform.position.y,directionToTarget.y) * moveSpeed;
-        }
-        else
-        {
-            gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0,0,0);
-        }*/
-    }
-    private void enemyDead()
-    {
-       
     }
     private void OnCollisionEnter(Collision collision)
     {
         //if a projectile collides with the enemy
-        if(collision.gameObject.tag == "projectile")
+        if (collision.gameObject.tag == "projectile")
         {
             //remove one health
             hp -= 1;
         }
     }
 }
-/*Vector2 enemyPosition = new Vector2(gameObject.transform.position.x, gameObject.transform.position.z);
-       Vector2 targetPosition = new Vector2(target.transform.position.x, target.transform.position.z);
-       Vector2 distanceToTarget = targetPosition - enemyPosition;
-       directionToTarget = distanceToTarget.normalized;
-       float numDistanceToTarget = distanceToTarget.magnitude;
-       if(numDistanceToTarget <= attackRange)
-       {
-           canAttack = true;
-       }
-       else
-       {
-           canAttack = false;
-       }*/
-
-
-/*agent.updateRotation = false;
-agent.updateUpAxis = false;*/
--- Assets/Scripts/roomSpawn.cs
+++ Assets/Scripts/roomSpawn.cs
@@ -37,9 +37,6 @@ public class roomSpawn : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //remove the room transition triggers
-        triggerSpawned = false;
-
         //setting up the values for the rooms 
         try
         {
@@ -48,41 +45,41 @@ public class roomSpawn : MonoBehaviour
             using (StreamReader reader = new StreamReader("Assets/Text_Files/map_layout.txt"))
             {
                 //reading the height and width from the first two lines
-                 floorHeight = int.Parse(reader.ReadLine());
-                 floorWidth = int.Parse(reader.ReadLine());
+                floorHeight = int.Parse(reader.ReadLine());
+                floorWidth = int.Parse(reader.ReadLine());
 
                 //sets size of array from the values
-                roomArray = new int[floorHeight,floorWidth];
-              
+                roomArray = new int[floorHeight, floorWidth];
+
                 //loop for floor height
                 for (int i = 0; i < floorHeight; i++)
                 {
                     //seperates the values splitting each line at a comma
                     string[] values = reader.ReadLine().Split(',');
-                    
+
                     //loop for the floor width
                     for (int h = 0; h < floorWidth; h++)
                     {
                         //sets the value of the coordinate in the array to the value from the text file
                         roomArray[i, h] = int.Parse(values[h]);
                         //display in the debug
-                     /*   Debug.Log(values[h]);*/
+                        /*   Debug.Log(values[h]);*/
                     }
                 }
             }
         }
-        catch(Exception e)
+        catch (Exception e)
         {
             Debug.Log(e);
         }
 
-     /*   Debug.Log($"{roomArray[0,0].ToString()}{roomArray[0, 1].ToString()}{roomArray[0, 2].ToString()}{roomArray[0, 3].ToString()}");
-        Debug.Log($"{roomArray[1, 0].ToString()}{roomArray[1, 1].ToString()}{roomArray[1, 2].ToString()}{roomArray[1, 3].ToString()}");
-        Debug.Log($"{roomArray[2, 0].ToString()}{roomArray[2, 1].ToString()}{roomArray[2, 2].ToString()}{roomArray[2, 3].ToString()}");
-        Debug.Log($"{roomArray[3, 0].ToString()}{roomArray[3, 1].ToString()}{roomArray[3, 2].ToString()}{roomArray[3, 3].ToString()}");
-        Debug.Log($"{roomArray[4, 0].ToString()}{roomArray[4, 1].ToString()}{roomArray[4, 2].ToString()}{roomArray[4, 3].ToString()}");
-        Debug.Log($"{roomArray[5, 0].ToString()}{roomArray[5, 1].ToString()}{roomArray[5, 2].ToString()}{roomArray[5, 3].ToString()}");
-    */
+        /*   Debug.Log($"{roomArray[0,0].ToString()}{roomArray[0, 1].ToString()}{roomArray[0, 2].ToString()}{roomArray[0, 3].ToString()}");
+           Debug.Log($"{roomArray[1, 0].ToString()}{roomArray[1, 1].ToString()}{roomArray[1, 2].ToString()}{roomArray[1, 3].ToString()}");
+           Debug.Log($"{roomArray[2, 0].ToString()}{roomArray[2, 1].ToString()}{roomArray[2, 2].ToString()}{roomArray[2, 3].ToString()}");
+           Debug.Log($"{roomArray[3, 0].ToString()}{roomArray[3, 1].ToString()}{roomArray[3, 2].ToString()}{roomArray[3, 3].ToString()}");
+           Debug.Log($"{roomArray[4, 0].ToString()}{roomArray[4, 1].ToString()}{roomArray[4, 2].ToString()}{roomArray[4, 3].ToString()}");
+           Debug.Log($"{roomArray[5, 0].ToString()}{roomArray[5, 1].ToString()}{roomArray[5, 2].ToString()}{roomArray[5, 3].ToString()}");
+       */
 
         //finds gameobject for room location
         //setting player coordinates to the start room location
@@ -101,21 +98,22 @@ public class roomSpawn : MonoBehaviour
 
         //finds gameobject for room location 
         roomLoc = GameObject.Find("roomOrigin(1,1)");
-       
+
     }
     //calls the instance the script is 
     void Awake()
     {
-       //creates an instance of the class to allow calling of variables from other classes
+        //creates an instance of the class to allow calling of variables from other classes
         roomScript = this;
         rSpawn();
     }
     void Update()
     {
-        if(Player_Movement.playerScript != null)
+        if (Player_Movement.playerScript != null)
         {
             roomValue = roomArray[Player_Movement.playerScript.playerY, Player_Movement.playerScript.playerX];
         }
+
         if (enemySpawn.spawnerScript != null)
         {
             //if enemies are in the room
@@ -129,21 +127,21 @@ public class roomSpawn : MonoBehaviour
                 roomClear = true;
             }
         }
-        //if room isn't clear
-        if (!roomClear)
-        {
-            if(!doorsSpawned)
+            //if room isn't clear
+            if (!roomClear)
             {
-                doorSpawn();
-                doorsSpawned = true;
-                triggerSpawned = false;
+                if (!doorsSpawned)
+                {
+                    doorSpawn();
+                    doorsSpawned = true;
+                    triggerSpawned = false;
+                }
             }
-        }
-        //if triggers haven't been spawned
+            //if triggers haven't been spawned
             //if room is clear
             if (roomClear)
             {
-                if(!triggerSpawned)
+                if (!triggerSpawned)
                 {
                     spawnTriggers();
                     triggerSpawned = true;
@@ -151,123 +149,110 @@ public class roomSpawn : MonoBehaviour
                 }
             }
     }
-    //spawning the room based on the prefab selected
-    public void rSpawn()
-    {
-        //finding which room should be spawned based on the values set in the array
-
-        if (roomValue == 1)
+        //spawning the room based on the prefab selected
+     public void rSpawn()
+     {
+         //finding which room should be spawned based on the values set in the array
+         if (roomValue == 1)
+         {
+             Debug.Log("Created Room 1");
+             //moving room to the position set by the roomLoc object
+             GameObject newRoom = Instantiate(startRoom);
+             newRoom.transform.position = roomLoc.transform.position;
+         }
+         else if (roomValue == 2)
+         {
+             Debug.Log("Created Room 2");
+             GameObject newRoom = Instantiate(treasureRoom);
+             newRoom.transform.position = roomLoc.transform.position;
+         }
+         else if (roomValue == 3)
+         {
+             Debug.Log("Created Room 3");
+             GameObject newRoom = Instantiate(bossRoom);
+             newRoom.transform.position = roomLoc.transform.position;
+         }
+         else if (roomValue == 4)
+         {
+             Debug.Log("Created Room 4");
+             GameObject newRoom = Instantiate(basicRoom);
+             newRoom.transform.position = roomLoc.transform.position;
+         }
+         else if (roomValue == 5)
+         {
+             Debug.Log("Created Room 5");
+             GameObject newRoom = Instantiate(basicRoom_2);
+             newRoom.transform.position = roomLoc.transform.position;
+         }
+         else if (roomValue == 6)
+         {
+             Debug.Log("Created Room 6");
+             GameObject newRoom = Instantiate(basicRoom_3);
+             newRoom.transform.position = roomLoc.transform.position;
+         }
+         else if (roomValue == 7)
+         {
+             Debug.Log("Created Room 7");
+             GameObject newRoom = Instantiate(basicRoom_4);
+             newRoom.transform.position = roomLoc.transform.position;
+         }
+         else
+         {
+             Debug.Log("Created Error Room");
+             GameObject newRoom = Instantiate(basicRoom_4);
+             newRoom.transform.position = roomLoc.transform.position;
+         }
+     }
+        //spawning triggers
+        private void spawnTriggers()
         {
-            Debug.Log("Created Room 1");
-            //moving room to the position set by the roomLoc object
-            GameObject newRoom = Instantiate(startRoom);
-            newRoom.transform.position = roomLoc.transform.position;
+            //spawning and translating the top trigger
+            GameObject topTrigger = Instantiate(pfTrigger);
+            topTrigger.name = "topTrigger";
+            topTrigger.transform.position = new Vector3(-5, 2, 110);
+            topTrigger.transform.localScale = new Vector3(20, 10, 9);
+            topTrigger.transform.Rotate(180, 0, 0);
+            //spawning and translating the bottom trigger
+            GameObject bottomTrigger = Instantiate(pfTrigger);
+            bottomTrigger.name = "bottomTrigger";
+            bottomTrigger.transform.position = new Vector3(-5, 2, -40);
+            bottomTrigger.transform.localScale = new Vector3(20, 10, 9);
+            bottomTrigger.transform.Rotate(180, 0, 0);
+            //spawning and translating the left trigger
+            GameObject leftTrigger = Instantiate(pfTrigger);
+            leftTrigger.name = "leftTrigger";
+            leftTrigger.transform.position = new Vector3(-80, 2, 35);
+            leftTrigger.transform.localScale = new Vector3(9, 10, 20);
+            //spawning and translating the right trigger
+            GameObject rightTrigger = Instantiate(pfTrigger);
+            rightTrigger.name = "rightTrigger";
+            rightTrigger.transform.position = new Vector3(70, 2, 35);
+            rightTrigger.transform.localScale = new Vector3(9, 10, 20);
+            //return to stop multiple triggers spawning
+            return;
         }
-        else if(roomValue == 2)
+        private void doorSpawn()
         {
-            Debug.Log("Created Room 2");
-            GameObject newRoom = Instantiate(treasureRoom);
-            newRoom.transform.position = roomLoc.transform.position;
+            //spawning and translating the left door
+            GameObject leftDoor = Instantiate(pfDoor);
+            leftDoor.transform.position = new Vector3(-80, 2, 35);
+            leftDoor.transform.localScale = new Vector3(10, 10, 20);
+            //spawning and translating the right door
+            GameObject rightDoor = Instantiate(pfDoor);
+            rightDoor.transform.position = new Vector3(70, 2, 35);
+            rightDoor.transform.localScale = new Vector3(10, 10, 20);
+            //spawning and translating the top door
+            GameObject topDoor = Instantiate(pfDoor);
+            topDoor.transform.position = new Vector3(-5, 2, 110);
+            topDoor.transform.localScale = new Vector3(20, 10, 10);
+            topDoor.transform.Rotate(180, 0, 0);
+            //spawning and translating the bottom door
+            GameObject bottomDoor = Instantiate(pfDoor);
+            bottomDoor.transform.position = new Vector3(-5, 2, -40);
+            bottomDoor.transform.localScale = new Vector3(20, 10, 10);
+            bottomDoor.transform.Rotate(180, 0, 0);
+            //return to stop multiple doors spawning
+            return;
         }
-        else if (roomValue == 3)
-        {
-            Debug.Log("Created Room 3");
-            GameObject newRoom = Instantiate(bossRoom);
-            newRoom.transform.position = roomLoc.transform.position;
-        }
-        else if (roomValue == 4)
-        {
-            Debug.Log("Created Room 4");
-            GameObject newRoom = Instantiate(basicRoom);
-            newRoom.transform.position = roomLoc.transform.position;
-        }
-        else if (roomValue == 5)
-        {
-            Debug.Log("Created Room 5");
-            GameObject newRoom = Instantiate(basicRoom_2);
-            newRoom.transform.position = roomLoc.transform.position;
-        }
-        else if (roomValue == 6)
-        {
-            Debug.Log("Created Room 6");
-            GameObject newRoom = Instantiate(basicRoom_3);
-            newRoom.transform.position = roomLoc.transform.position;
-        }
-        else if (roomValue == 7)
-        {
-            Debug.Log("Created Room 7");
-            GameObject newRoom = Instantiate(basicRoom_4);
-            newRoom.transform.position = roomLoc.transform.position;
-        }
-        else
-        {
-            // set the players x and y back to the spawn room 
-
-            for (int i = 0; i < floorHeight; i++)
-            {
-                //loop for the floor width
-                for (int h = 0; h < floorWidth; h++)
-                {
-                    if (roomArray[i, h] == 1)
-                    {
-                        Player_Movement.playerScript.playerX = h;
-                        Player_Movement.playerScript.playerY = i;
-                        GameObject newRoom = Instantiate(startRoom);
-                        newRoom.transform.position = roomLoc.transform.position;
-                    }
-                }
-            }
-        }
-    }
-    //spawning triggers
-    private void spawnTriggers()
-    {
-        //spawning and translating the top trigger
-        GameObject topTrigger = Instantiate(pfTrigger);
-        topTrigger.name = "topTrigger";
-        topTrigger.transform.position = new Vector3(-5, 2, 110);
-        topTrigger.transform.localScale = new Vector3(20, 10, 9);
-        topTrigger.transform.Rotate(180, 0, 0);
-        //spawning and translating the bottom trigger
-        GameObject bottomTrigger = Instantiate(pfTrigger);
-        bottomTrigger.name = "bottomTrigger";
-        bottomTrigger.transform.position = new Vector3(-5, 2, -40);
-        bottomTrigger.transform.localScale = new Vector3(20, 10, 9);
-        bottomTrigger.transform.Rotate(180, 0, 0);
-        //spawning and translating the left trigger
-        GameObject leftTrigger = Instantiate(pfTrigger);
-        leftTrigger.name = "leftTrigger";
-        leftTrigger.transform.position = new Vector3(-80, 2, 35);
-        leftTrigger.transform.localScale = new Vector3(9, 10, 20);
-        //spawning and translating the right trigger
-        GameObject rightTrigger = Instantiate(pfTrigger);
-        rightTrigger.name = "rightTrigger";
-        rightTrigger.transform.position = new Vector3(70, 2, 35);
-        rightTrigger.transform.localScale = new Vector3(9, 10, 20);
-        //return to stop multiple triggers spawning
-        return;
-    }
-    private void doorSpawn()
-    {
-        //spawning and translating the left door
-        GameObject leftDoor = Instantiate(pfDoor);
-        leftDoor.transform.position = new Vector3(-80, 2, 35);
-        leftDoor.transform.localScale = new Vector3(10, 10, 20);
-        //spawning and translating the right door
-        GameObject rightDoor = Instantiate(pfDoor);
-        rightDoor.transform.position = new Vector3(70, 2, 35);
-        rightDoor.transform.localScale = new Vector3(10, 10, 20);
-        //spawning and translating the top door
-        GameObject topDoor = Instantiate(pfDoor);
-        topDoor.transform.position = new Vector3(-5, 2, 110);
-        topDoor.transform.localScale = new Vector3(20, 10, 10);
-        topDoor.transform.Rotate(180,0,0);
-        //spawning and translating the bottom door
-        GameObject bottomDoor = Instantiate(pfDoor);
-        bottomDoor.transform.position = new Vector3(-5, 2, -40);
-        bottomDoor.transform.localScale = new Vector3(20, 10, 10);
-        bottomDoor.transform.Rotate(180, 0, 0);
-        //return to stop multiple doors spawning
-        return;
-    }
 }
+
