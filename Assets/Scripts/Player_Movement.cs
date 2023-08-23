58cd833357d7f832e3a6ff1df98e91d2f0d80916
BrodyBrownlee <brodybrownlee@mmc.school.nz>
2023-08-24 11:01:29 +1200

    Simplified Code

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -42,127 +42,56 @@ public class Player_Movement : MonoBehaviour
     //player collision with triggers
     void OnTriggerEnter(Collider other)
     {
-        if(other.name == "leftTrigger")
+        if(other.tag == "trigger")
         {
-            Debug.Log("Left Trigger");
-            //subtracting one from roomX
-            if (room.roomScript != null)
+            if (room.roomScript != null && wall.wallScript != null)
             {
                 Debug.Log("room changed");
                 room.roomScript.roomChange();
-            }
-            //calling the roomChange method for the wall class
-            if (wall.wallScript != null)
-            {
+                //calling the roomChange method for the wall class
                 Debug.Log("walls changed");
                 wall.wallScript.roomChange();
             }
-            playerX--;
             Cr.enabled = false;
-            Cr.transform.position = new Vector3(65, 5, Cr.transform.position.z);
-            Cr.enabled = true;
-/*            //calls the room has been changed
-            wall.wallScript.roomChange();*/
-            if (roomSpawn.roomScript != null)
-            {
-                if (room.roomScript.roomChanged == true)
-                {
-                    roomSpawn.roomScript.roomValue = roomSpawn.roomScript.roomArray[Player_Movement.playerScript.playerY, Player_Movement.playerScript.playerX];
-                    roomSpawn.roomScript.rSpawn();
-                }
-            }
-        }
-        else if (other.name == "rightTrigger")
-        {
-         //calling the roomChange method for the room class  
-            if (room.roomScript != null)
-            {
-                Debug.Log("room changed");
-                room.roomScript.roomChange();
-            }
-            //calling the roomChange method for the wall class
-            if(wall.wallScript != null)
-            {
-                Debug.Log("walls changed");
-                wall.wallScript.roomChange();
-            }
-            //adding one to roomX
-            playerX++;
-            Debug.Log("right Trigger");
-            //stops character controller from preventing tp
-            Cr.enabled = false;
-            Cr.transform.position = new Vector3(-65, 5, Cr.transform.position.z);
-            Cr.enabled = true;
-         /*   //calls the room has been changed
-            wall.wallScript.roomChange();*/
-            if (roomSpawn.roomScript != null)
-            {
-                if (room.roomScript.roomChanged == true)
-                {
-                    roomSpawn.roomScript.roomValue = roomSpawn.roomScript.roomArray[Player_Movement.playerScript.playerY, Player_Movement.playerScript.playerX];
-                    roomSpawn.roomScript.rSpawn();
-                }
-            }
-        }
-        else if (other.name == "topTrigger")
-        {
-            Debug.Log("top Trigger");
-            if (room.roomScript != null)
-            {
-                Debug.Log("room changed");
-                room.roomScript.roomChange();
-            }
-            //calling the roomChange method for the wall class
-            if (wall.wallScript != null)
-            {
-                Debug.Log("walls changed");
-                wall.wallScript.roomChange();
-            }
-            //moving one room up(reversed because of 2d array)
-            playerY--;
-            //stops character controller from preventing tp
-            Cr.enabled = false;
-            Cr.transform.position = new Vector3(Cr.transform.position.x, 5, -65);
-            Cr.enabled = true;
-            if (roomSpawn.roomScript != null)
-            {
-                if (room.roomScript.roomChanged == true)
-                {
-                    roomSpawn.roomScript.roomValue = roomSpawn.roomScript.roomArray[Player_Movement.playerScript.playerY, Player_Movement.playerScript.playerX];
-                    roomSpawn.roomScript.rSpawn();
-                }
-            }
+            switch (other.name){
 
-
-            //adding one to roomY
-        }
-        else if (other.name == "bottomTrigger")
-        {
-            Debug.Log("bottom Trigger");
-            if (room.roomScript != null)
-            {
-                Debug.Log("room changed");
-                room.roomScript.roomChange();
+                case "rightTrigger":
+                    //adding one to roomX
+                    playerX++;
+                    Debug.Log("right Trigger");
+                    //stops character controller from preventing tp
+                    Cr.transform.position = new Vector3(-65, 5, Cr.transform.position.z);
+                    break;
+                case "leftTrigger":
+                    //adding one to roomX
+                    playerX--;
+                    Debug.Log("left Trigger");
+                    //stops character controller from preventing tp
+                    Cr.transform.position = new Vector3(65, 5, Cr.transform.position.z);
+                    break;
+                case "topTrigger":
+                    //moving one room up(reversed because of 2d array)
+                    playerY--;
+                    Debug.Log("top Trigger");
+                    //stops character controller from preventing tp
+                    Cr.transform.position = new Vector3(Cr.transform.position.x, 5, -65);
+                    break;
+                case "bottomTrigger":
+                    Debug.Log("bottom Trigger");
+                    //moving one room down
+                    playerY++;
+                    //stops character controller from preventing tp
+                    Cr.transform.position = new Vector3(Cr.transform.position.x, 5, 65);
+                    break;
             }
-            //calling the roomChange method for the wall class
-            if (wall.wallScript != null)
-            {
-                Debug.Log("walls changed");
-                wall.wallScript.roomChange();
-            }
-            //moving one room down
-            playerY++;
-            //stops character controller from preventing tp
-            Cr.enabled = false;
-            Cr.transform.position = new Vector3(Cr.transform.position.x, 5, 65);
             Cr.enabled = true;
+            //calls the room has been changed
             if (roomSpawn.roomScript != null)
             {
-                if(room.roomScript.roomChanged == true)
+                if (room.roomScript.roomChanged == true)
                 {
-                     //sets the room value before changing the room so that the room is properly updated
-                     roomSpawn.roomScript.roomValue = roomSpawn.roomScript.roomArray[Player_Movement.playerScript.playerY, Player_Movement.playerScript.playerX];
-                     roomSpawn.roomScript.rSpawn();
+                    roomSpawn.roomScript.roomValue = roomSpawn.roomScript.roomArray[Player_Movement.playerScript.playerY, Player_Movement.playerScript.playerX];
+                    roomSpawn.roomScript.rSpawn();
                 }
             }
         }
